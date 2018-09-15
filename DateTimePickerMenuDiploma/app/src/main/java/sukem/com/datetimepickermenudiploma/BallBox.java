cb1f074d2fda6300088034646d0f5087f9e2e6ca
Solankimimoh <solankimimoh@gmail.com>
2018-09-15 11:28:30 +0530

    menuDateTimePickerDiploma

---
+++ DateTimePickerMenuDiploma/app/src/main/java/sukem/com/datetimepickermenudiploma/BallBox.java
@@ -0,0 +1,7 @@
+package sukem.com.datetimepickermenudiploma;
+
+public interface BallBox {
+
+    public void max(int a,int b);
+
+}
---
+++ DateTimePickerMenuDiploma/app/src/main/java/sukem/com/datetimepickermenudiploma/MainActivity.java
@@ -0,0 +1,140 @@
+package sukem.com.datetimepickermenudiploma;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText dateEd;
+    private EditText timeEd;
+    private Button dateBtn;
+    private Button timeBtn;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+    private int mHour;
+    private int mMinute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+    }
+
+    private void initView() {
+        dateEd = findViewById(R.id.activity_main_date_ed);
+        dateBtn = findViewById(R.id.activity_main_date_btn);
+        timeEd = findViewById(R.id.activity_main_time_ed);
+        timeBtn = findViewById(R.id.activity_main_time_btn);
+
+        timeBtn.setOnClickListener(this);
+        dateBtn.setOnClickListener(this);
+
+        registerForContextMenu(dateBtn);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.activity_main_date_btn:
+                openDatePicker();
+                break;
+            case R.id.activity_main_time_btn:
+                openTimePicker();
+                break;
+        }
+    }
+
+    private void openTimePicker() {
+
+        Calendar calendar = Calendar.getInstance();
+
+        mHour = calendar.get(Calendar.HOUR);
+        mMinute = calendar.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                timeEd.setText(hourOfDay + ":" + minute);
+            }
+        }, mHour, mMinute, false);
+        timePickerDialog.show();
+    }
+
+    private void openDatePicker() {
+
+        Calendar calendar = Calendar.getInstance();
+
+        mYear = calendar.get(Calendar.YEAR);
+        mMonth = calendar.get(Calendar.MONTH);
+        mDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                dateEd.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+            }
+        }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_setting:
+                Toast.makeText(this, "" + item.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_about:
+                Toast.makeText(this, "" + item.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.home_menu,menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_setting:
+                Toast.makeText(this, "" + item.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_about:
+                Toast.makeText(this, "" + item.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+}
